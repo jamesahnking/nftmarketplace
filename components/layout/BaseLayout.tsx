37ef070735e03b04313f592014ade2c86fd33e03
jamesahnking <jamesahnking@users.noreply.github.com>
2022-06-02 14:06:12 -0400

    - Added css for base layout children container.

--- components/layout/BaseLayout.tsx
+++ components/layout/BaseLayout.tsx
@@ -8,7 +8,7 @@ interface Props {
 const BaseLayout: FunctionComponent<Props> = ({children}) => {
   return (
     <div className="py-16 bg-slate-700 overflow-hidden min-h-screen">
-        <div className="">
+        <div className="max-w-7xl mx-auto px-4 space-y-8 sm:px-6 lg:px-8">
             {children}
         </div>
     </div>
