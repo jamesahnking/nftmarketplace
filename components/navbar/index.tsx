3ba33223c39b88221f379857ebc09b704bb4a837
jamesahnking <jamesahnking@users.noreply.github.com>
2022-06-07 13:23:58 -0400

    - Edded navigation to two items - Added links to navigation

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -3,15 +3,14 @@
 import { Fragment } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline'
+// 
 
 const navigation = [
-  { name: 'Dashboard', href: '#', current: true },
-  { name: 'Team', href: '#', current: false },
-  { name: 'Projects', href: '#', current: false },
-  { name: 'Calendar', href: '#', current: false },
+  { name: 'NFT Marketplaces', href: '/', current: true },
+  { name: 'Create NFTs', href: '/nft/create', current: false },
 ]
 
-function classNames(...classes: string[]) {
+  function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(' ')
 }
 
