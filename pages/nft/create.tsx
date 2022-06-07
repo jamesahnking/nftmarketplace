5c68e18512fd4a9dba15ebabb9ce34f775948063
jamesahnking <jamesahnking@users.noreply.github.com>
2022-06-07 13:08:50 -0400

    - Created nft page director - Added 'create' page component

---
+++ pages/nft/create.tsx
@@ -0,0 +1,16 @@
+// Create NFT 
+
+import type { NextPage } from 'next';
+import { BaseLayout } from '../../components';
+
+const NftCreate: NextPage = () => {
+  return (
+   
+     <BaseLayout>
+    Make NFTS
+    </BaseLayout>       
+    
+    )
+}
+
+export default NftCreate
