106c42aa30e08f53007a49fa648635a160cd406f
jamesahnking <jamesahnking@users.noreply.github.com>
2022-06-07 13:53:39 -0400

    - Added z-index 10 to popup profile menu to correct 'Sign Out' from being cut off.

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -72,7 +72,7 @@ export default function Navbar() {
                 </button>
 
                 {/* Profile dropdown */}
-                <Menu as="div" className="ml-3 relative">
+                <Menu as="div" className="ml-3 relative z-10">
                   <div>
                     <Menu.Button className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
                       <span className="sr-only">Open user menu</span>
