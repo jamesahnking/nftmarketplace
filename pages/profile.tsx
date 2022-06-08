075865a54184f0815061195c6db3d16df20c0c1c
jamesahnking <jamesahnking@users.noreply.github.com>
2022-06-08 08:56:19 -0400

    - Created Profile page component - Added profile link to navbar - Removed Logout and Settings from profile pop up menu

---
+++ pages/profile.tsx
@@ -0,0 +1,16 @@
+// Create NFT 
+
+import type { NextPage } from 'next';
+import { BaseLayout } from '../components';
+
+const Profile: NextPage = () => {
+  return (
+   
+     <BaseLayout>
+Profile page
+    </BaseLayout>       
+    
+    )
+}
+
+export default Profile
