6ca849a80689f76c50e1adf0a4a0a494d67aa329
jamesahnking <jamesahnking@users.noreply.github.com>
2022-06-08 11:20:17 -0400

    - Replaced Link with ActiveLink functionality in the navbar - Added key and href from Link to ActiveLink - Added activeClass to ActiveLink

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -3,7 +3,7 @@
 import { Fragment } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline'
-import Link from 'next/link'
+import ActiveLink from '../link'
 // 
 
 const navigation = [
@@ -44,20 +44,18 @@ export default function Navbar() {
                 <div className="hidden sm:block sm:ml-6">
                   <div className="flex space-x-4">
                     {navigation.map((item) => (
-                      <Link 
+                      <ActiveLink
                         key={item.name} 
-                        href={item.href}>
+                        href={item.href}
+                        activeClass="bg-gray-900 text-white"
+                      >
                       <a
-                        className={classNames(
-                          item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                          'px-3 py-2 rounded-md text-sm font-medium'
-                        )}
+                        className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
                         aria-current={item.current ? 'page' : undefined}
                       >
                         {item.name}
                       </a>
-                     
-                      </Link>
+                      </ActiveLink>
                     ))}
                   </div>
                 </div>
@@ -95,15 +93,12 @@ export default function Navbar() {
                     <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
                       <Menu.Item>
                         {({ active }) => (
-                        <Link
-                          href="/profile"
-                        >
                           <a
+                            href="/profile"
                             className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
                           >
                             Your Profile
                           </a>
-                        </Link>
                         )}
                       </Menu.Item>
            
