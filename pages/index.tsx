b67808a40558ce56d6ac764d94a3e0b0764e5d75
jamesahnking <jamesahnking@users.noreply.github.com>
2022-06-12 10:58:43 -0400

    - Added useWeb3 - test to the application homepage that will print "hellow web3 provider"

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,14 @@
 import type { NextPage } from 'next';
-import { BaseLayout, NftList } from '../components/ui';
+import { BaseLayout, NftList } from '@ui';
 import nfts from "../content/meta.json";
-import { NftMeta } from "@_types/nft";
+import { NftMeta } from '@_types/nft';
+import { useWeb3 } from '@providers/web3';
 // @dev - This is your applications homepage 
 const Home: NextPage = () => {
+
+  // inject web3
+  const data = useWeb3();
+
   return (
     <BaseLayout>
       <div className="relative bg-gray-50 pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
