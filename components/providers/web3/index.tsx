fe698b199af20ffe3b644392261c60c00398744a
jamesahnking <jamesahnking@users.noreply.github.com>
2022-06-15 08:59:21 -0400

    - Exported web3 types for ethereum, provider, and contract.

--- components/providers/web3/index.tsx
+++ components/providers/web3/index.tsx
@@ -1,6 +1,14 @@
 import { createContext, useContext, useState } from "react";
 import { FunctionComponent } from "react";
+import { MetaMaskInpageProvider } from "@metamask/providers";
+import { Contract, providers } from "ethers";
 
+// Export Web3 types - ? allowing undefined 
+export type Web3State = {
+  ethereum?: MetaMaskInpageProvider;
+  provider?: providers.Web3Provider;
+  contract?: Contract;
+}
 
 // The web3 provider will wrap all components in _app.tsx
 const Web3Context = createContext<any>(null);
@@ -11,7 +19,7 @@ interface Props {
 
 const Web3Provider: FunctionComponent<Props> = ({children}) => {
 
-  const [web3Api, setWeb3Api] = useState({test: "Hello Web3 Provider!"})
+  const [web3Api, setWeb3Api] = useState( {test: "Hello Web3 Provider!"} )
     
   return (
     <Web3Context.Provider value={web3Api}>
