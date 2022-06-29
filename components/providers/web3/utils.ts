f60543823131ec9cec71963a0fd991497b6d53f0
jamesahnking <jamesahnking@users.noreply.github.com>
2022-06-29 09:18:25 -0400

    - Added contract loading functinality to web3 utills  - Added loading contract functionalityi to web3 wrapper  - Added dotenv dependency

--- components/providers/web3/utils.ts
+++ components/providers/web3/utils.ts
@@ -1,5 +1,7 @@
+require('dotenv').config();
 import { MetaMaskInpageProvider } from "@metamask/providers";
-import { Contract, providers } from "ethers";
+import { Contract, providers,ethers } from "ethers";
+
 
 
 // global window.ethereum definition 
@@ -17,7 +19,7 @@ export type Web3Params= {
     contract: Contract | null;
   }
 
-// export params ant types 
+// export params and types 
 export type Web3State = {
     isLoading: boolean; //true wulile loading webState
 } & Web3Params
@@ -33,3 +35,51 @@ export const createDefaultState = () => {
         isLoading: true,
     }
 }
+
+
+
+
+// Load contract into Next JS
+
+// import network contract lives on 
+const NETWORK_ID = process.env.NEXT_PUBLIC_NETWORK_ID;
+
+// name: name of contract
+// provider: Web3 ethers provider
+
+export const loadContract = async (
+    name: string, //Nft contract name
+    provider: providers.Web3Provider
+    ): Promise<Contract> => {
+    // create instance of contract
+
+    // do you have the network id? 
+    if (!NETWORK_ID) {
+        //reject
+        return Promise.reject(" A network ID is not defined!");
+    }
+    //otherwise fetch the contracts json representation
+    const res = await fetch(`/contracts/${name}.json`);
+    //store the file
+    const Artifact = await res.json(); 
+
+    // do you have a network address where the contract lives? 
+    if (Artifact.networks[NETWORK_ID].address) {
+        // load the contract
+        // 1. address
+        // 2. abi
+        // 3. provdier
+        const contract = new ethers.Contract(
+            Artifact.networks[NETWORK_ID].address,
+            Artifact.abi,
+            provider
+        )
+        return contract; 
+    }  else {
+        // if you dont have an address
+        return Promise.reject(`Contract: [${name}] cannot be loaded!`);
+
+    }
+}
+
+
