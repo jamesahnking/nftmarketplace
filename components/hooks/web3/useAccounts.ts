2a8810036cc39ecddbbe24870ddf0e36bbc79f12
jamesahnking <jamesahnking@users.noreply.github.com>
2022-07-02 19:31:31 -0400

    - Added CryptoHookFactory - Added generic typing for data and parameter to definitions for all the 'Crypto' defined hooks.

--- components/hooks/web3/useAccounts.ts
+++ components/hooks/web3/useAccounts.ts
@@ -1,16 +1,25 @@
 // Generate Next JS Webhooks 
+import { CryptoHookFactory } from "@_types/hooks";
 import useSWR from "swr";
-import { debug } from "util";
-// Hi   
-// Dependencies are:
+
+// The name “SWR” is derived from stale-while-revalidate, 
+// a HTTP cache invalidation strategy popularized by HTTP RFC 5861. 
+// SWR is a strategy to first return the data from cache (stale), then 
+// send the fetch request (revalidate), and finally come with the up-to-date data.
+
+type AccountHookFactory = CryptoHookFactory<string, string>
+
+export type UseAccountHook = ReturnType<AccountHookFactory>
+
+
+// hookFactory dependencies are:
 // provider
 // ethereum
 // contract (web3State)
 // a function that returns a function
 
-export const hookFactory = (deps: any) => (params: any) => {
+export const hookFactory: AccountHookFactory = (deps) => (params) => {
     const swrRes = useSWR("web3/useAccount", () => {
-        debug
         console.log(deps);
         console.log(params);
         return "Test User"
@@ -19,5 +28,5 @@ export const hookFactory = (deps: any) => (params: any) => {
     return swrRes;
 }
 
-export const useAccount = hookFactory({ethereum: null, provider: null});
+// export const useAccount = hookFactory({ethereum: undefined, provider: undefined});
 
--- types/hooks.ts
+++ types/hooks.ts
@@ -3,21 +3,17 @@ import { MetaMaskInpageProvider } from "@metamask/providers";
 import { Contract, providers } from "ethers";
 import { SWRResponse } from "swr";
 
-
+// Web3 needs
 export type Web3Dependencies = {
     provider: providers.Web3Provider;
     contract: Contract;
     ethereum: MetaMaskInpageProvider;
 }
 
+// Accepts web3 dependencies 
+export type CryptoHookFactory = {(d: Partial <Web3Dependencies>):CryptoHandlerHook; }
+// Handler funt to return SWRResponse
+export type CryptoHandlerHook = (params: any) => CryptoSWRResponse;
 // Rename SWRResponse
 export type CryptoSWRResponse = SWRResponse;
 
-// Handler funt to return SWRResponse
-export type CryptoHandlerHook = (params: any) => CryptoSWRResponse;
-
-// Accepts web3 dependencies 
-// Returns 
-export type CryptoHookFactory = {
-    (d: Partial <Web3Dependencies>):CryptoHandlerHook;
-}
