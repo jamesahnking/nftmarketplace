d6935552d769df63e58620d48a315015aef9d705
jamesahnking <jamesahnking@users.noreply.github.com>
2022-07-13 10:16:53 -0400

    - Added the Ganache network as an environment variable - Added targetNetwork and isSupported to the useNetworkResponse

--- components/hooks/web3/useNetwork.ts
+++ components/hooks/web3/useNetwork.ts
@@ -5,39 +5,40 @@ import { CryptoHookFactory } from "@_types/hooks";
 import useSWR from "swr";
 
 type UseNetworkResponse = {
-    connect: () => void;
     isLoading: boolean;
-    isInstalled: boolean;
+    isSupported: boolean;
+    targetNetwork: string;
 }
 
 type NetworkHookFactory = CryptoHookFactory<string, UseNetworkResponse>
-
 export type UseNetworkHook = ReturnType<NetworkHookFactory>
 
-export const hookFactory: NetworkHookFactory = ({provider, isLoading}) => () => {
+// get chain ID mapping - k is the network number
+const NETWORK: {[k: string]: string} = {
+    1: "Ethereum MainNetwork",
+    3: "Ropsten Testnet",
+    4: "Rinkeby Testnet",
+    5: "Goerli Testnet",
+    56: "Binance Smart Chain",
+    1337: "Ganache",
+}
+
+const targetId = process.env.NEXT_PUBLIC_TARGET_CHAIN_ID as string;
+const targetNetwork = NETWORK[targetId];
+
+
+export const hookFactory: NetworkHookFactory = ({provider, isLoading,}) => () => {
    const {data, isValidating, ...swrRes} = useSWR(
         provider ? "web3/useNetwork" : null, 
           async () => {
 
-            // get chain ID mapping
-            // k is the network number
-            const NETWORK: {[k: string]: string} = {
-                1: "Ethereum MainNetwork",
-                3: "Ropsten Testnet",
-                4: "Rinkeby Testnet",
-                5: "Goerli Testnet",
-                56: "Binance Smart Chain",
-                1337: "Ganache",
-            }
-
-            const chainId = (await provider!.getNetwork()).chainId
+            const chainId = (await provider!.getNetwork()).chainId;
             
             if(!chainId) {
-                throw "Cannot retrieve network. Please, refresh browser or try connecting to another network."
+                throw "Cannot retrieve the Ganache network. Please, refresh browser or try connecting to another network."
             }
             // *
             return NETWORK[chainId];   
-            
         }, 
             // Update the data when refocusing the window 
             { revalidateOnFocus: false 
@@ -48,6 +49,8 @@ export const hookFactory: NetworkHookFactory = ({provider, isLoading}) => () => 
         ...swrRes, //hookFactory
         data, //hookFactory
         isValidating, //hookFactory
+        targetNetwork,
+        isSupported: data === targetNetwork,
         isLoading: isLoading || isValidating, //hookFactory
     };
 }
