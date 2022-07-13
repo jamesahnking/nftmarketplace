6b596687120af573c8ebde436af8057e743d901f
jamesahnking <jamesahnking@users.noreply.github.com>
2022-07-13 10:35:19 -0400

    - Added Network loading logic for the network loading. - If the network present and loading display loading else ask user to install a wallet

--- components/ui/navbar/index.tsx
+++ components/ui/navbar/index.tsx
@@ -86,9 +86,14 @@ export default function Navbar() {
                     <svg className="-ml-0.5 mr-1.5 h-2 w-2 text-indigo-400" fill="currentColor" viewBox="0 0 8 8">
                       <circle cx={4} cy={4} r={3} />
                     </svg>
-                    {network.data} ---
-                    {`Is supported: ${network.isSupported}`} ---
-                    Target: {network.targetNetwork}
+                    {/* If network is loading display loading otherwise tell the user to install a wallet */}
+                   
+                    {network.isLoading ? 
+                    "Loading..." : account.isInstalled ? 
+                    network.data:
+                    "Install Web3 Wallet"
+                    }
+                  
                   </span>
                 </div>       
 
