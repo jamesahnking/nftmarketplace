a806fae3649b22758eb561bef5e0c83baa83eaed
jamesahnking <jamesahnking@users.noreply.github.com>
2022-07-26 15:14:46 -0400

    - Removed comments

--- test/commands.js
+++ test/commands.js
@@ -1,10 +1,5 @@
-// run 
-// node commands.js
-
-
 // Deploy Contract
 const instance = await NftMarket.deployed();
-
 // Fuurzlz 1
 instance.mintToken("https://gateway.pinata.cloud/ipfs/QmaitkDtdBZToPrMhMRTivDRNQnLCtgmUzrowTzuHtu6qv","500000000000000000", {value: "25000000000000000",from: accounts[0]});
 // Fuurzlz 2
