cad5d0c0c6d4b8401f8f9a7cd3cfb78ba4add142
jamesahnking <jamesahnking@users.noreply.github.com>
2022-08-14 13:50:12 -0400

    - Added addressCheckMiddleware to uitils.ts to hadline checking address against the session - Added addressCheckMiddleware to verify.ts that will use it to check the validity of the form data

--- pages/api/verify.ts
+++ pages/api/verify.ts
@@ -1,23 +1,24 @@
 import { v4 as uuidv4 } from "uuid";
 import { Session } from "next-iron-session";
 import { NextApiRequest, NextApiResponse } from "next";
-import { withSession, contractAddress } from "./utils";
+import { withSession, contractAddress, addressCheckMiddleware } from "./utils";
 import { NftMeta } from "@_types/nft";
 
-// generate set save and return session back with message
+// Generate set save and return session back with message
 export default withSession (async(req: NextApiRequest & 
     {session: Session}, res: NextApiResponse) => {
         if (req.method === "POST") {
             try {
                 const {body} = req;
                 const nft = body.nft as NftMeta
-                // verify / check if nft has been created
+                // Verify / Check if nft has been created
                 if (!nft.name || !nft.description || !nft.attributes) {
-                    res.status(422).send({message: "Some of the form data are missing!"});
+                    return res.status(422).send({message: "Some of the form data are missing!"});
                     }
-                    res.status(200).send({message: "NFT has been created"});
+                    await addressCheckMiddleware(req, res);
+                    return res.status(200).send({message: "NFT has been created"});
                 } catch {
-                    res.status(422).send({message: "Cannot create JSON"});
+                    return res.status(422).send({message: "Cannot create JSON"});
                     }
         } else if (req.method === "GET") {
             try {
